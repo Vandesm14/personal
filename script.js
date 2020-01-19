8999a72e4d786d10be961500ec72c4fa96c8ef06
Vandesm14 <vandesm14@gmail.com>
2020-01-18 20:25:56 -0500

    finalize basic animations

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-$(document).ready(function(){
-	$('.part-A, .part-C').addClass('done');
-	setTimeout(function(){
-		$('.part-B, .animation').addClass('done');
-	}, 500);
+var scrollLimit = $(window).height() / 1;
+
+$(document).ready(function () {
+	$('.part-A, .part-B').removeClass('start');
+	setTimeout(function () {
+		$('.part-C').removeClass('start');
+	}, 600);
+
+	$('.scroll').on('scroll', function () {
+		let percent = $('.scroll').scrollTop() / scrollLimit;
+		$('.nav *').css('opacity', percent);
+	});
 });
