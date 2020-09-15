3c15d346425726fea5377aa12aba90d919170b85
vandesm14 <vandesm14@gmail.com>
2020-09-15 19:59:06 -0400

    add social buttons and fix animations

--- script.js
+++ script.js
@@ -1,30 +1,47 @@
 let slide = 0;
 
 $(document).ready(function () {
-	// $('#projects > .item').on('mousedown', function (e) {
-	$('#projects').on('mousedown', function (e) {
+	$('#projects-container').on('mousedown', function (e) {
 		e.preventDefault();
 		let x = e.clientX;
-		let width = $(document).width();
+		let width = $(window).width();
 		if (x < width / 2) { // Left
-			if (slide > 0) {
-				slide--;
-				$('#projects').animate({
-					marginLeft: `${-slide*100+100}vw`
-				}, {duration: 500, easing: 'easeInOutQuad'});
-			}
+			move(-1);
 		} else if (x > width / 2) { // Right
-			if (slide < 2) {
-				slide++;
-				$('#projects').animate({
-					marginLeft: `${-slide*100+100}vw`
-				}, {duration: 500, easing: 'easeInOutQuad'});
-			}
+			move(1);
+		}
+	});
+
+	$(document).on('keydown', function (e) {
+		if ($('#projects-container').is(':focus')) return;
+		if (e.key === 'ArrowLeft') {
+			move(-1);
+		} else if (e.key === 'ArrowRight') {
+			move(1);
 		}
 	});
 
 	$('#projects > .item *').on('mousedown', function (e) {
-		console.log('child click');
 		e.stopPropagation();
 	});
 });
+
+function move(by) {
+	if (by === 1) {
+		if (slide < $('#projects').children().length - 1) {
+			slide++;
+			$('#projects').stop(true, true);
+			$('#projects').animate({
+				marginLeft: `${-slide*100+100}vw`
+			}, {duration: 500, easing: 'easeInOutQuad'});
+		}
+	} else if (by === -1) {
+		if (slide > 0) {
+			slide--;
+			$('#projects').stop(true, true);
+			$('#projects').animate({
+				marginLeft: `${-slide*100+100}vw`
+			}, {duration: 500, easing: 'easeInOutQuad'});
+		}
+	}
+}
